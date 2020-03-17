38ba14261f96d629af11bcb5e8ee490b4e282360
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-03-17 17:50:41 -0500

    done!

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,12 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  // console.log(props.comments);
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {props.comments.map(p => ( 
+        <Comment comment={p} />
+      ))}
       <CommentInput />
     </div>
   );
